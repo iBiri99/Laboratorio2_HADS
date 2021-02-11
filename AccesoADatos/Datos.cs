f2938d30b25052fd37013fb3273f1d1c6ec6b9de
iBiri99 <biribari99@hotmail.com>
2021-02-11 12:14:04 +0100

    Mas cambios

--- AccesoADatos/Datos.cs
+++ AccesoADatos/Datos.cs
@@ -9,14 +9,14 @@ namespace AccesoADatos
 {
     public class Datos
     {
-        string connetionString;
-        SqlConnection cnn;
+        private SqlConnection cnn;
+        private SqlCommand command;
         //Devuelve 0 si se ha hecho correctamente la conexion y 1 si se ha hecho correctamente.
         public int AbrirSesion()
         {
             try
             {
-                connetionString = @"Server=tcp:hads21-15.database.windows.net,1433;Initial Catalog=HADS21-15;Persist Security Info=False;User ID=hads2021@outlook.es@hads21-15;Password=Pass1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                string connetionString = @"Server=tcp:hads21-15.database.windows.net,1433;Initial Catalog=HADS21-15;Persist Security Info=False;User ID=hads2021@outlook.es@hads21-15;Password=Pass1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
                 cnn = new SqlConnection(connetionString);
                 cnn.Open();
                 return 0;
@@ -29,8 +29,25 @@ namespace AccesoADatos
         //Funcion que saca si el correo esta en la base de datos.
         public int comprobarCorreo(String correo)
         {
-            SqlCommand command = cnn.CreateCommand();
-            command.CommandText ="Select ";
+            command = new SqlCommand("Select count(email) from Usuarios where email=", cnn);
+            command.CommandText ="Select count(email) from Usuarios where email="+correo;
+
+            Console.WriteLine(command.ExecuteScalar());
+            Console.WriteLine("HOLAAAAAAAAAAAAAAAAAAAAAA");
+            return 0;
+        }
+
+        public int cerrarConexion()
+        {
+            try
+            {
+                cnn.Close();
+                return 0;
+            }
+            catch
+            {
+                return 1;
+            }
         }
 
     }
--- Laboratorio2/Inicio.aspx.cs
+++ Laboratorio2/Inicio.aspx.cs
@@ -10,15 +10,36 @@ namespace Laboratorio2
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        AccesoADatos.Datos bd;
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+/*
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+
+           int a=bd.cerrarConexion();
+            if (a == 0)
+            {
+                ScriptManager.RegisterClientScriptBlock(Page, typeof(Page), "ClientScript", "alert('hello')", true);
+            }
+            else
+            {
+
+            }
+        }
+*/
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-                 AccesoADatos.Datos bd = new AccesoADatos.Datos();
-                 int a=bd.AbrirSesion();
+            bd = new AccesoADatos.Datos();
+            int a=bd.AbrirSesion();
+            if (a == 0)
+            {
+                ScriptManager.RegisterClientScriptBlock(Page, typeof(Page), "ClientScript", "alert('La conexion se ha realizado correctamente.')", true);
+                bd.comprobarCorreo("hola");
+            }
         }
     }
 }
